c891cbf23d402413e13219c581c17e8730299915
Oleh Sikorskyi <helgi.sikora@gmail.com>
2018-10-08 11:53:52 +0300

    Create CSV and Excel Data Prpvider Class

---
+++ README.md
@@ -0,0 +1,6 @@
+
+Data Driven Testing
+Lesson 02
+
+Read User Data From Xml and CSV File TestNG DataProvider 
+
---
+++ src/test/java/com/epam/lab/DeleteInboxMessageTest.java
@@ -0,0 +1,44 @@
+package com.epam.lab;
+
+import com.epam.lab.businessobjects.InboxPageBO;
+import com.epam.lab.businessobjects.LoginPageBO;
+import com.epam.lab.utils.dataproviders.ExcelDataProviderClass;
+import com.epam.lab.utils.parsers.ConfigFileReader;
+import com.epam.lab.utils.WebDriverPool;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import org.testng.annotations.*;
+
+public class DeleteInboxMessageTest {
+    private ConfigFileReader configFileReader;
+    public static int initialInboxEmailQuantity;
+
+    @BeforeClass
+    public void setupDriver() {
+        configFileReader = new ConfigFileReader();
+        System.setProperty("webdriver.chrome.driver", configFileReader.getDriverPath());
+    }
+
+    //When you will use CSV Data Provider to parse User data - uncomment next line
+    //@Test(dataProvider = "csvDataProvider", dataProviderClass = CsvDataProviderClass.class)
+    @Test(dataProvider = "ExcelDataProvider", dataProviderClass = ExcelDataProviderClass.class)
+    public void deleteInboxMessage(String login, String password) {
+        WebDriver driver = WebDriverPool.getDriver();
+        driver.get(configFileReader.getWebSiteUrl());
+        LoginPageBO loginPageBO = new LoginPageBO(driver);
+        loginPageBO.loginToGmail(login, password);
+        InboxPageBO inboxPageBO = new InboxPageBO(driver);
+        initialInboxEmailQuantity = inboxPageBO.checkInboxEmailQaontity();
+        inboxPageBO.selectInboxEmail();
+        inboxPageBO.deleteEmail();
+        inboxPageBO.cancelDeletingEmail();
+        Assert.assertEquals(initialInboxEmailQuantity, inboxPageBO.checkInboxEmailQaontity());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        WebDriverPool.removeDriver();
+    }
+}
